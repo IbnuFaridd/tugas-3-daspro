e5e19d267e9438e3e0fbb0e7ccf5150ff6f91e43
IbnuFaridd <113011904+IbnuFaridd@users.noreply.github.com>
2022-09-26 18:47:35 +0700

    Add files via upload

---
+++ tugas 3 daspro/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float p,l;
+
+    cout <<"program luas persegi panjang" << endl;
+    cout <<"masukkan panjang : ";
+    cin >> p;
+    cout <<"masukkan lebar : ";
+    cin >>l;
+    cout <<"luas persegi pamjang adalah " << p*l;
+
+    return 0;
+}
